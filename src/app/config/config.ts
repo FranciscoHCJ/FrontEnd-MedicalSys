048d0f9da81f17fe44641f2489af9ab1d9eeea79
Hipolito Castillo Juan Francisco <juanfranciscoutn@gmail.com>
2019-03-24 17:43:31 -0600

    Autenticación Funcionando con Guard

---
+++ src/app/config/config.ts
@@ -0,0 +1,7 @@
+
+
+
+export const URL_SERVICIOS = 'http://localhost:3000';
+
+
+;
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -9,18 +9,20 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 
 
+import { LoginGuardGuard } from '../services/service.index';
 
 const pagesRoutes: Routes = [
     {
         path: '',
         component: PagesComponent,
+        canActivate: [ LoginGuardGuard],
         children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: 'promesas', component: PromesasComponent },
-            { path: 'rxjs', component: RxjsComponent },
-            { path: 'account-settings', component: AccoutSettingsComponent },
+            { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+            { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
+            { path: 'account-settings', component: AccoutSettingsComponent, data: { titulo: 'Ajustes del Tema' } },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
         ]
     }
